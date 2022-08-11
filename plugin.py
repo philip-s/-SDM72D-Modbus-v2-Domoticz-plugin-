69935bfca7b2300d60ff16a3ace48719778a6e7d
philip-s <49447850+philip-s@users.noreply.github.com>
2022-08-11 19:15:50 +0200

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -109,9 +109,9 @@ class BasePlugin:
         # if 19 not in Devices:
         #     Domoticz.Device(Name="Average line to neutral volts", Unit=19,Type=0xF3,Subtype=0x8,Used=0).Create()
         # Options = { "Custom" : "1;V"} 
-        if 20 not in Devices: 
-            Domoticz.Device(Name="Average line current", Unit=20,Type=0xF3,Subtype=0x17,Used=0).Create()
-        Options = { "Custom" : "1;A"} 
+        # if 20 not in Devices: 
+        #     Domoticz.Device(Name="Average line current", Unit=20,Type=0xF3,Subtype=0x17,Used=0).Create()
+        # Options = { "Custom" : "1;A"} 
         if 21 not in Devices:
             Domoticz.Device(Name="Sum of line currents", Unit=21,Type=0xF3,Subtype=0x17,Used=0).Create()
         Options = { "Custom" : "1;A"}
@@ -202,7 +202,7 @@ class BasePlugin:
             L2_Power_Factor = self.rs485.read_float(32, functioncode=4, numberOfRegisters=2)
             L3_Power_Factor = self.rs485.read_float(34, functioncode=4, numberOfRegisters=2)
             # Average_line_to_neutral_volts = self.rs485.read_float(42, functioncode=4, numberOfRegisters=2)
-            Average_line_current = self.rs485.read_float(46, functioncode=4, numberOfRegisters=2)
+            # Average_line_current = self.rs485.read_float(46, functioncode=4, numberOfRegisters=2)
             Sum_of_line_currents = self.rs485.read_float(48, functioncode=4, numberOfRegisters=2)
             Total_System_Power = self.rs485.read_float(52, functioncode=4, numberOfRegisters=2)
             # Total_System_Volt_amps = self.rs485.read_float(56, functioncode=4, numberOfRegisters=2)
@@ -245,7 +245,7 @@ class BasePlugin:
             Devices[17].Update(0,str(L2_Power_Factor))
             Devices[18].Update(0,str(L3_Power_Factor))
             # Devices[19].Update(0,str(Average_line_to_neutral_volts))
-            Devices[20].Update(0,str(Average_line_current))
+            # Devices[20].Update(0,str(Average_line_current))
             Devices[21].Update(0,str(Sum_of_line_currents))
             Devices[22].Update(0,str(Total_System_Power))
             # Devices[23].Update(0,str(Total_System_Volt_amps))
